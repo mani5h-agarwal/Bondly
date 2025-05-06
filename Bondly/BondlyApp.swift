77f15bbbaf6aa32cb318106725a058fce6e6e415
Manish <manishagarwal428728@gmail.com>
2025-05-06 22:24:23 +0530

    Initial Commit

---
+++ Bondly/BondlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BondlyApp.swift
+//  Bondly
+//
+//  Created by Manish Agarwal on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BondlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BondlyTests/BondlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BondlyTests.swift
+//  BondlyTests
+//
+//  Created by Manish Agarwal on 06/05/25.
+//
+
+import Testing
+@testable import Bondly
+
+struct BondlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
